28ae23582a74280f16b849c96373308c1aa7e95a
Randoooom <random@randoms.rocks>
2022-01-21 16:04:58 +0100

    feat: setup nuxt config

--- nuxt/nuxt.config.js
+++ nuxt/nuxt.config.js
@@ -6,29 +6,25 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - asapm',
-    title: 'asapm',
+    titleTemplate: '%s - ASAPM',
+    title: 'ASAPM',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -47,14 +43,59 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
+    '@nuxtjs/auth-next',
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {},
+  axios: {
+    baseURL:
+      process.env.NODE_ENV === 'production'
+        ? 'https://asapm.randoooom.workers.dev'
+        : 'http://localhost:8787',
+  },
+
+  // apply auth
+  router: {
+    middleware: ['auth'],
+  },
+
+  auth: {
+    resetOnError: true,
+    localStorage: false,
+    cookie: {
+      options: {
+        sameSite: 'Strict',
+        secure: 'true',
+      },
+    },
+    strategies: {
+      local: {
+        user: false,
+        scheme: 'local',
+        token: {
+          property: 'token',
+          maxAge: 1800,
+        },
+        endpoints: {
+          login: { url: '/auth/login', method: 'post' },
+          logout: false,
+          user: false,
+        },
+      },
+    },
+    redirect: {
+      login: '/login',
+      logout: '/login',
+      home: '/',
+    },
+    fullPathRedirect: true,
+  },
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
+    treeShake: true,
     customVariables: ['~/assets/variables.scss'],
+    icons: { iconFont: 'md' },
     theme: {
       dark: true,
       themes: {
@@ -65,13 +106,20 @@ export default {
           info: colors.teal.lighten1,
           warning: colors.amber.base,
           error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
-    }
+          success: colors.green.accent3,
+        },
+      },
+    },
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  }
+    friendlyErrors: false,
+  },
+
+  generate: {
+    fallback: true,
+  },
+
+  loading: false,
 }
