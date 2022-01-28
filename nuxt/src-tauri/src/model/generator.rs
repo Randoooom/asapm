0364c145dd0029e4eb8e578a14e4d48e63dea6d2
Randoooom <53190645+Randoooom@users.noreply.github.com>
2022-01-28 23:40:41 +0100

    feat: basic password generator

--- nuxt/src-tauri/src/model/generator.rs
+++ nuxt/src-tauri/src/model/generator.rs
@@ -23,7 +23,49 @@
  * SOFTWARE.
  */
 
-pub mod encryption;
-pub mod user;
-pub mod backup;
-pub mod generator;
+pub struct PasswordGenerator {
+  numbers: bool,
+  letters: bool,
+  symbols: bool,
+}
+
+impl Default for PasswordGenerator {
+  fn default() -> Self {
+    Self {
+      numbers: true,
+      letters: true,
+      symbols: true,
+    }
+  }
+}
+
+impl PasswordGenerator {
+  pub fn generate(&self, length: usize) -> String {
+    // setup dataset
+    let mut dataset = String::from("");
+    // add letters
+    if self.letters {
+      dataset.push_str("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    }
+    // add numbers
+    if self.numbers {
+      dataset.push_str("0123456789")
+    }
+    // add symbols
+    if self.symbols {
+      dataset.push_str("!@#$%^&*()-_+/")
+    }
+
+    // parse to char vec
+    let dataset = dataset.chars().collect::<Vec<char>>();
+
+    // generate random numbers
+    let numbers = (0..length).map(|_| { rand::random::<u8>() }).collect::<Vec<u8>>();
+
+    // iter through the numbers and add push the matching char into the password
+    let password = numbers.iter().map(|number| { dataset[*number as usize % &dataset.len()] }).collect::<String>();
+
+    password
+  }
+}
+
